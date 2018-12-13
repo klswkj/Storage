56be7c23c2850ba97d4b2e109662860871dac603
klswkj <40312730+klswkj@users.noreply.github.com>
2018-12-13 15:29:17 +0900

    Update DC3.h

--- Header/DC3.h
+++ Header/DC3.h
@@ -1,29 +1,14 @@
-#ifndef ManberMyers_Included
-#define ManberMyers_included
+#ifndef DC3_Included
+#define DC3_Included
 
-#prgma once
-#include <vector>
-#include <cstddef> // For size_t
+#pragma once
+#include "ManberMyers.h"
 
-
-/**
- * Type : SuffixArray
- *
- * A type represeting a suffix array.
- */
-
- /* This implementation adopts the custom that the input string is an array of
- * integers representing the relative ranks of the characters in the original
- * string. It's assumed that the original string was suffixed by a special
- * sentinel character $ that appears nowhere else in the string. For example,
- * if the true string to build a suffix array for is ABRACADABRA, we'd begin by
- * suffixing a $ terminator to get ABRACADABRA$, then replace each character
- * with its rank ($ = 0, A = 1, B = 2, C = 3, D = 4, R = 5) to get 125131412510
- * as our input.
- */
-
-using SuffixArray = std::vector<std::size_t>;
-
-SuffixArray ManberMyers(const std::vector<size_t> &text);
+/* Computes a suffix array using the DC3 algorithm. As with the Manber-Myers
+* algorithm, this implementation assumes that the input text string has been
+* suffixed with a sentinel and had each of its characters remapped to its rank
+* among all characters.
+*/
+SuffixArray dc3(const std::vector<std::size_t> &text);
 
 #endif
