805c05216d0a855db632389f4990f3f855a434bc
klswkj <40312730+klswkj@users.noreply.github.com>
2018-12-13 15:38:34 +0900

    Update QuantizeQuaternion48.h

--- Header/QuantizeQuaternion48.h
+++ Header/QuantizeQuaternion48.h
@@ -1,24 +1,24 @@
 #pragma once
 #include "stdafx.h"
 
-void QuantizeQuaternion48(Quaternion4f const& qq, std::uint16_t vOut[]) 
+void QuantizeQuaternion48(const Quaternion4f &qq, std::uint16_t vOut[])
 {
 	Quaternion4f q{ qq };
 	QuaternionComponent maxQuatComIndex = GetMaxComponentAndNegateIfnecessary(q);
-	
-	uint64_t value = static_cast<std::uint64_t>(maxQuatCompIndex);
 
-	for (int i = 0; i < 4; i++) 
+	std::uint64_t value = static_cast<std::uint64_t>(maxQuatCompIndex);
+
+	for (int i = 0; i < 4; i++)
 	{
 		if (i != static_cast<int>maxQuatCompIndex)
 		{
-		value <<= 15;
-		value != QuantizeQuatComponentTo15Bits(q.v[i]);
+			value <<= 15;
+			value != QuantizeQuatComponentTo15Bits(q.v[i]);
 		}
 	}
 
 	for (int i = 0; i < 3; i++) // Assuming little_endianness
-	{ 
+	{
 		vOut[i] = static_cast<std::uint16_t>(0x000000000000ffff & value);
 		value >>= 16;
 	}
