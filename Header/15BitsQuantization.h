48a79eb3130e5dc61a17d3f163fce0a6e1e26a6b
klswkj <40312730+klswkj@users.noreply.github.com>
2018-12-13 16:01:47 +0900

    Update 15BitsQuantization.h

--- Header/15BitsQuantization.h
+++ Header/15BitsQuantization.h
@@ -1,25 +1,24 @@
 #pragma once
-
+#include "stdafx.h"
 
 float const kRootTwo = 1.41421356237f;
 int const kMaxInFifteenBits = (1 << 15) - 1;
 float const kOneOverRootTwo = 1.f / kRootTwo;
 float const kDefaultEpsilon = 0.00001f;
 
-float PrepareQuatComponentForQuantization(float v) 
+float PrepareQuatComponentForQuantization(float v)
 {
 	return (v + kOneOverRootTwo) * kOneOverRootTwo;
 }
 
-std::uint64_t QuantizeQuatComponentTo15Bits(float v) 
+std::uint64_t QuantizeQuatComponentTo15Bits(float v)
 {
 	float const vv = prepareQuatComponentForQuantization(v);
 	return static_cast<std::uint64_t>(
 		std::max(std::min(static_cast<int>(vv * static_cast<float> (kMaxInFifteenBits)), kMaxInFifteenBits), 0));
 }
 
-float Dequantize15BitQuatComponent(std::uint16_t v) 
+float Dequantize15BitQuatComponent(std::uint16_t v)
 {
 	return static_cast<float>(v) / static_cast<float>(kMaxInFifteenBits)* kRootTwo - kOneOverRootTwo;
 }
-
